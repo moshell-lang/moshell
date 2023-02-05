ffc3b866ac4373f357d72c2115be569be5282a3f
Override-6 <maximebatista18@gmail.com>
2023-02-05 15:52:28 +0100

    removed idea folder

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,36 @@
+use std::fs;
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    #[token("(")]
+    LeftRoundBracket,
+    #[token(")")]
+    RightRoundBracket,
+    #[token("[")]
+    LeftSquareBracket,
+    #[token("]")]
+    RightSquareBracket,
+    #[token("fun")]
+    Fun,
+    #[token("\\S+")]
+    Any(str),
+
+    #[error]
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Error
+}
+
 fn main() {
-    let contents
-    println!("{}", var);
+    let contents = fs::read_to_string("sample.msh")
+        .expect("Cannot open file sample.msh");
+
+    let mut lexer = Token::lexer(&contents);
+
+    let mut token = lexer.next();
+    while token != None {
+        token = lexer.next();
+    }
+
 }
 
