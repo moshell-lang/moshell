4b3de0d75ee76d53bb9cf539729c84cc595eb018
syldium <syldium@mailo.com>
2023-03-03 21:37:08 +0100

    Add a CLI to test the parser

---
+++ cli/src/main.rs
@@ -0,0 +1,64 @@
+use context::source::Source;
+use miette::{Diagnostic, GraphicalReportHandler, NamedSource, SourceSpan};
+use parser::err::ParseErrorKind;
+use parser::parse;
+use std::io::{self, BufRead};
+use thiserror::Error;
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("Parse error")]
+struct FormattedError {
+    #[source_code]
+    src: NamedSource,
+    #[label("Here")]
+    cursor: SourceSpan,
+    #[label("Start")]
+    related: Option<SourceSpan>,
+    message: String,
+    #[help]
+    help: Option<String>,
+}
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let handler = GraphicalReportHandler::default();
+
+    while let Some(line) = lines.next() {
+        let line = line?;
+        let report = parse(Source::new(&line, "stdin"));
+        let errors = report
+            .errors
+            .into_iter()
+            .map(|err| FormattedError {
+                src: NamedSource::new("stdin", line.clone()),
+                cursor: err.position.into(),
+                related: match &err.kind {
+                    ParseErrorKind::Unpaired(pos) => Some(pos.clone().into()),
+                    _ => None,
+                },
+                message: err.message,
+                help: match &err.kind {
+                    ParseErrorKind::Excepted(excepted) => Some(format!("Excepted: {:?}", excepted)),
+                    _ => None,
+                },
+            })
+            .collect::<Vec<_>>();
+
+        if errors.is_empty() {
+            println!("{:?}", report.expr);
+            continue;
+        }
+        let mut msg = String::new();
+        for err in &errors {
+            if let Err(fmt_err) = handler.render_report(&mut msg, err) {
+                eprintln!("{fmt_err}");
+            } else {
+                eprintln!("{msg}");
+            }
+        }
+    }
+
+    Ok(())
+}
