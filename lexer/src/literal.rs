6a76e55142dfe6a63d53ef11a4773da5fcf5ed97
syldium <syldium@mailo.com>
2023-04-01 21:05:33 +0200

    Move backslash handling out of the parser

--- lexer/src/literal.rs
+++ lexer/src/literal.rs
@@ -75,12 +75,24 @@ impl<'a> Lexer<'a> {
 
     pub(crate) fn next_space(&mut self, start_pos: usize, start_char: char) -> Token<'a> {
         let mut pos = start_pos + start_char.len_utf8();
-        while let Some((p, c)) = self.iter.peek() {
-            if *c != ' ' && *c != '\t' {
+        while let Some((p, c)) = self.iter.peek().copied() {
+            if c == '\\' {
+                let mut it = self.iter.clone();
+                it.next();
+                if let Some((p, c)) = it.next() {
+                    if c.is_whitespace() {
+                        pos = p + c.len_utf8();
+                        self.iter = it;
+                        continue;
+                    }
+                }
+                break;
+            } else if c == ' ' || c == '\t' {
+                pos = p + c.len_utf8();
+                self.iter.next();
+            } else {
                 break;
             }
-            pos = p + c.len_utf8();
-            self.iter.next();
         }
         Token::new(TokenType::Space, &self.input[start_pos..pos])
     }
