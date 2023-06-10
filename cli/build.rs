f61c014833c5ee1e615437dd4106734c68adc88c
syldium <syldium@mailo.com>
2023-06-10 14:13:53 +0200

    Work around change detections

--- cli/build.rs
+++ cli/build.rs
@@ -1,17 +1,17 @@
-use std::env;
 use cmake::Config;
+use std::env;
 
 fn main() {
-        let dst = Config::new("../vm").build();
-        println!("cargo:rustc-link-search=native={}", dst.display());
-        println!("cargo:rustc-link-lib=static=vm");
+    let dst = Config::new("../vm").build();
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=vm");
 
-        let target = env::var("TARGET").unwrap();
-        if target.contains("apple") {
-                println!("cargo:rustc-link-lib=dylib=c++");
-        } else if target.contains("linux") {
-                println!("cargo:rustc-link-lib=dylib=stdc++");
-        } else {
-                panic!("unsupported target {target}")
-        }
+    let target = env::var("TARGET").unwrap();
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    } else if target.contains("linux") {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    } else {
+        panic!("unsupported target {target}")
+    }
 }
