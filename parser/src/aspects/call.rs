29ee40337ade7f3a0aed706000576900e32fee69
syldium <syldium@mailo.com>
2023-07-07 22:46:38 +0200

    Remove unused method

--- parser/src/aspects/call.rs
+++ parser/src/aspects/call.rs
@@ -25,9 +25,6 @@ pub trait CallAspect<'a> {
     /// Parses a raw call, a programmatic call or a lambda definition.
     fn any_call(&mut self) -> ParseResult<Expr<'a>>;
 
-    /// Parses a process call where the command name is only known at runtime.
-    fn dyn_call(&mut self) -> ParseResult<Expr<'a>>;
-
     /// Attempts to parse the next raw call expression
     fn call(&mut self) -> ParseResult<Expr<'a>>;
 
@@ -110,12 +107,6 @@ impl<'a> CallAspect<'a> for Parser<'a> {
         } else {
             self.call_arguments(path, callee, type_parameters)
         }
-    }
-
-    fn dyn_call(&mut self) -> ParseResult<Expr<'a>> {
-        self.cursor
-            .force(of_type(TokenType::Shell), "Expected 'dyn' keyword.")?;
-        self.call()
     }
 
     fn call(&mut self) -> ParseResult<Expr<'a>> {
